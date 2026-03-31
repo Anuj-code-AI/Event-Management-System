a4f58295edc551b0722a0656fb07c41d705df4d7
Anuj Chourasiya <achourasiyaanuj@gmail.com>
2026-03-31 15:16:58 +0530

    Remove dotenv setup from main application
    Removed dotenv configuration and environment variable injection.

--- src/main/java/org/anuj/EvenTAura/EvenTAuraApplication.java
+++ src/main/java/org/anuj/EvenTAura/EvenTAuraApplication.java
@@ -8,13 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EvenTAuraApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-
-		// Inject ALL .env variables into system properties
-		// so Spring can resolve ${...} placeholders
-		dotenv.entries().forEach(entry ->
-				System.setProperty(entry.getKey(), entry.getValue())
-		);
 		SpringApplication.run(EvenTAuraApplication.class, args);
 	}
 
