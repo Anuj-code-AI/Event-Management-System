2ea3bd4cc31253ca73336cc249676af9c7fce744
Anuj-Code-AI <achourasiyaanuj@gmail.com>
2026-03-31 15:35:26 +0530

    fix

--- src/main/java/org/anuj/EvenTAura/EvenTAuraApplication.java
+++ src/main/java/org/anuj/EvenTAura/EvenTAuraApplication.java
@@ -1,6 +1,5 @@
 package org.anuj.EvenTAura;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EvenTAuraApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EvenTAuraApplication.class, args);
 	}
 
